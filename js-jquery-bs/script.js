dfb660b08f4ac5fadd6aa7ff6c3beed1d37494c6
Osvaldo <osv_93@hotmail.es>
2017-03-27 21:08:49 -0600

    avance

--- js-jquery-bs/script.js
+++ js-jquery-bs/script.js
@@ -4,76 +4,83 @@
 //http://www.variablenotfound.com/2011/12/funciones-constructoras-en-javascript.html
 
 
-    function Cliente(nombre, fecha) {
+    function usuario(nombre, fecha) {
  
         //Propiedades privadas
         var edad;
  
         //Métodos privados
         var calcularEdad = function() {
-            var actual = new Date().getYear();
-            var nacimiento = fecha.getYear();
+            var actual = new Date().getFullYear();
+            var nacimiento = fecha.getFullYear();
  
             if (actual <= nacimiento)
                 edad = "Error: no se ha podido calcular";
             else
                 edad = actual - nacimiento;
+            	
         };
  
         //Propiedades públicas
         this._nombre = nombre;
         this._fechaNacimiento = fecha;
         this._edad = edad;
-        this._direccion = direccion;
  
         //Métodos públicos
         this._presentarse = function() {
             calcularEdad();
- 
-            document.write(
-                "Hola, mi nombre es " + this._nombre + 
-                " y tengo " + this._edad + " años."
-            );
+            var result = "Hola, mi nombre es "+ nombre +" y tengo " + edad + " años"
+            var estilo = $("#mostrarResultado").css('background', 'yellow');
+ 			mostrarResultados(result, estilo);
         };
     }
-
-
-
-
-    function Constructor(msjPrivado, msjPublico) {
- 
-        var propiedadPrivada = msjPrivado;
-        this.propiedadPublica = msjPublico;
- 
-        var metodoPrivado = function () {
-            alert(propiedadPrivada);
-            alert(this.propiedadPublica);
-        };
- 
-        this.metodoPublico = function () {
-            metodoPrivado();
-        };
-    }
- 
-    var obj = new Constructor("mensaje privado", "mensaje público");
-    obj.metodoPublico();
-
-
-
-
-
-
-
-
-
 
 $(document).ready(function(){
 	$("#btn_calcular_edad").click(function() {
-	  $("#mostrarResultado").text(new Date());
-	  mostrarResultados("hola","");
+
+	var nombre = $("#name").val();
+	var fecha_nacimiento = $("#fecha_nacimiento").val();
+
+    var user = new usuario(nombre, new Date(fecha_nacimiento));
+	});
+});
+
+/*$(document).ready(function(){
+	$("#btn_azul").on('click','li',function (){
+	    $("#mostrarResultado").text($(this).text());
+	    $("#mostrarResultado").css('background', 'blue');
+	});
+});*/
+
+$(document).ready(function(){
+	$("#btn_azul").click(function() {
+		$("#mostrarResultado").css('background', 'blue');
+	});
+});
+$(document).ready(function(){
+	$("#btn_rojo").click(function() {
+		$("#mostrarResultado").css('background', 'red');
+	});
+});
+
+$(document).ready(function(){
+	$("#btn_verde").click(function() {
+		$("#mostrarResultado").css('background', 'green');
+	});
+});
+
+$(document).ready(function(){
+	$("#btn_amarillo").click(function() {
+		$("#mostrarResultado").css('background', 'yellow');
+	});
+});
+
+$(document).ready(function(){
+	$("#btn_rosado").click(function() {
+		
 	});
 });
 
 var mostrarResultados = function(texto, estilo) {
-	alert(texto);
+	$("#mostrarResultado").text(texto);
 }
